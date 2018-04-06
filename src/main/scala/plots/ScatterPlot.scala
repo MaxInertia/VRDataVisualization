ff10238d8320a6468c90b3dd04afba707d062e75
Dorian Thiessen (dbt908) <dorian.thiessen@usask.ca>
2018-04-06 17:00:14 -0500

    Update ScatterPlot
    Build should now pass

--- src/main/scala/plots/ScatterPlot.scala
+++ src/main/scala/plots/ScatterPlot.scala
@@ -1,17 +1,36 @@
 package plots
 
 import org.scalajs.{threejs => THREE}
+import plots.ScatterPlot.CoordinateAxisIDs
 
 /**
+  * Container of a simple scatter-plot.
+  *
   * Created by Dorian Thiessen on 2018-04-05.
   */
-class ScatterPlot(tag: String, points: THREE.Points) extends Plot(tag, points)
+class ScatterPlot(tag: String, points: THREE.Points) extends Plot(tag, points) {
+  private var xid: CoordinateAxisIDs = _
+  private var yid: CoordinateAxisIDs = _
+  private var zid: CoordinateAxisIDs = _
+}
 
+// Tried some unconventional spacing on this one. I have mixed feelings about it.
 object ScatterPlot {
-  def apply(title: String,     hue: Double = 0.0,
-            xs: Array[Double], ys: Array[Double], zs: Array[Double],
-            xid: String,       yid: String,       zid: String): ScatterPlot = {
+  type CoordinateAxisIDs = String
+
+  def apply(title: String,          hue: Double = 0.0,
+            xs: Array[Double],      ys: Array[Double],      zs: Array[Double],
+            xid: CoordinateAxisIDs, yid: CoordinateAxisIDs, zid: CoordinateAxisIDs): ScatterPlot = {
     val coordinates: Array[Coordinate] = Plot.zip3(xs, ys, zs)
-    new ScatterPlot(title, Plot.makePoints(coordinates, hue))
+    val scatterPlot = new ScatterPlot(title, Plot.makePoints(coordinates, Some(hue), 0))
+    setAxisIDs(scatterPlot, xid, yid, zid)
+    scatterPlot
+  }
+
+  private def setAxisIDs(sp: ScatterPlot,      x: CoordinateAxisIDs,
+                         y: CoordinateAxisIDs, z: CoordinateAxisIDs): Unit = {
+    sp.xid = x
+    sp.yid = y
+    sp.zid = z
   }
 }
