456c107ae0972ee3e8bfe5e9f5ece2a46bdf503b
Dorian Thiessen (dbt908) <dorian.thiessen@usask.ca>
2018-04-11 02:06:19 -0500

    Actually add the Log object -_-

---
+++ src/main/scala/util/Log.scala
@@ -0,0 +1,18 @@
+package util
+
+import org.scalajs.dom
+
+import scala.scalajs.js
+
+/**
+  * Created by Dorian Thiessen on 2018-04-10.
+  */
+object Log {
+  val showLogs: Boolean = false
+
+  def apply(message: String): Unit =
+    if(showLogs) dom.console.log(message)
+
+  def apply(anything: js.Any): Unit =
+    if(showLogs) dom.console.log(anything)
+}
